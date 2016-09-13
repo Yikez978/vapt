5604dfa72d7b6628ecab371bcc88763ad5b1d039
Angelis Pseftis <anpseftis@users.noreply.github.com>
2016-09-13 15:03:14 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -62,6 +62,7 @@ EOKEY
 
   echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /home/dev/.ssh/config
   chmod 600 /home/dev/.ssh/*
+  git clone git@github.com:anpseftis/vapt.git
   curl -#LO https://rvm.io/mpapis.asc
   gpg --import mpapis.asc
   curl -sSL https://get.rvm.io | bash -s stable
