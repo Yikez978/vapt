d580743e84c761de18338422e63b27fce375db4b
angelispseftis <anpseftis86@gmail.com>
2016-09-16 13:00:20 -0400

    Removing more old stuff

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,9 +4,11 @@ class SettingsController < ApplicationController
 	def edit
 		@categories = Setting.uniq.pluck(:category).sort
 		@settings = Setting.all.order(category: :asc, id: :asc)
+		@brackets = Bracket.all.order('priority ASC')
 		@users = User.all
+		@teams = Team.all
 	end
-	
+
 	def update
 		error = false
 		settings = params[:admin]
@@ -15,7 +17,7 @@ class SettingsController < ApplicationController
 			to_update = Setting.find(setting[:id])
 
 			if to_update.setting_type == 'boolean'
- 				to_update.value = (setting[:value]) ? "1" : "0"
+				to_update.value = (setting[:value]) ? "1" : "0"
 			else
 				to_update.value = setting[:value]
 			end
@@ -32,11 +34,11 @@ class SettingsController < ApplicationController
 	end
 
 	private
-		def admin_user
-      unless logged_in? && current_user.admin?
-				store_location
-				flash[:danger] = "Access Denied."
-				redirect_to root_url
-			end
-    end
+	def admin_user
+		unless logged_in? && current_user.admin?
+			store_location
+			flash[:danger] = "Access Denied."
+			redirect_to root_url
+		end
+	end
 end
